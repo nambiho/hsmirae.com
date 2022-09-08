095570fffa9764a9f1baed6b84c5ebcf0d80d900
nambiho.moon <nambiho@naver.com>
2022-09-08 16:29:04 +0900

    new version

---
+++ src/components/Nav.svelte
@@ -0,0 +1,35 @@
+<script>
+</script>
+
+<header class="p-2 text-bg-yellow">
+	<div class="container">
+		<div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+			<a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+				<img src="/assets/images/logos/HSMIRAE_LOGO_white_nobg.png" class="nav-logo" alt="">
+			</a>
+
+			<ul class="nav nav-custom col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+				<li><a href="#" class="nav-link px-2 text-secondary">Home</a></li>
+				<li><a href="#" class="nav-link px-2 text-dark">Features</a></li>
+				<li><a href="#" class="nav-link px-2 text-dark">Product</a></li>
+				<li><a href="#" class="nav-link px-2 text-dark">About</a></li>
+			</ul>
+		</div>
+	</div>
+</header>
+
+<style lang="scss">
+	.nav-custom {
+		padding-left: revert;
+	}
+
+	.text-bg-yellow {
+    color: #fff!important;
+    // background-color: RGBA(33,37,41,var(--bs-bg-opacity,1))!important
+		background-color: #ffdd00!important;
+	}
+
+	.nav-logo {
+		height: 80px;
+	}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,31 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import sveltePreprocess from 'svelte-preprocess';
+import path from 'path';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()]
-})
+const production = process.env.NODE_ENV === 'production';
+const config = defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  plugins: [
+    svelte({
+      emitCss: production,
+			preprocess: sveltePreprocess(),
+			compilerOptions: {
+				dev: !production,
+			},
+			hot: !production
+    })
+  ],
+  server: {
+    host: 'localhost',
+    port: 5000
+  }
+});
+
+console.log(config);
+
+export default config;
